7a62eadca2739fe3fe1dff14bf7f54fc699e930d
hamiltonh <hamiltonhernandez@gmail.com>
2021-07-23 00:50:08 -0400

    Clase 6 21072021

--- Project1/src/main/java/com/misiontic/project1/Exercise1.java
+++ Project1/src/main/java/com/misiontic/project1/Exercise1.java
@@ -65,7 +65,6 @@ public class Exercise1 {
         } else {
             System.out.println("No se encontraron coincidencias. " + nombreABuscar);
         }
-        
     }
     
     public Person[] searchFullName(String fullName){
